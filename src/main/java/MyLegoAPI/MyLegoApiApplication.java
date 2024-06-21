8cf702e5eeeea82064ba4250f7080de6da6be243
Daniel Martín <danisamsevilla@gmail.com>
2024-06-22 00:10:19 +0200

    Initial Spring Init Skeleton

---
+++ src/main/java/MyLegoAPI/MyLegoApiApplication.java
@@ -0,0 +1,13 @@
+package MyLegoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLegoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLegoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MyLegoAPI/MyLegoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package MyLegoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLegoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
